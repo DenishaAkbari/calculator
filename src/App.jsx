ac6b97b6ce1a75056571ff63f7c4f14e9f894bfa
DenishaAkbari <denishaakbari1136@gmail.com>
2024-08-21 12:41:26 +0530

    r-calculator

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useRef } from 'react';
+import Calculator from './components/cals/calc';
+import './app.css';
+
+const App = () => {
+    return (
+        <>
+        <h1>Calculator</h1>
+          <Calculator />
+        </>
+    );
+};
+
+export default App;
